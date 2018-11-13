d62419cf8e64d06c9ad03e273a71d5776aa01ce3
Boyan Georgiev <boyan_georgiev88@abv.bg>
2018-11-13 14:35:43 +0200

    configurations for tests of manual type mapper

--- src/test/java/com/github/chaosfirebolt/mapper/ManualTypeMapperTests.java
+++ src/test/java/com/github/chaosfirebolt/mapper/ManualTypeMapperTests.java
@@ -51,6 +51,47 @@ public class ManualTypeMapperTests extends AbstractTypeMapperTests {
                 .mapping(Teacher.class, TeacherView.class).composer()
                 .transform(STUDENT_LIST_CLASS, STUDENT_VIEW_LIST_CLASS).supplier(Teacher::getStudents).consumer(TeacherView::setStudents)
                 .function(list -> list.stream().map(student -> super.getTypeMapper().map(student, StudentView.class)).collect(Collectors.toList()))
+                    .finish()
+                .mapping(Company.class, CompanyView.class).composer()
+                .transform(String.class, String.class).supplier(Company::getName).consumer(CompanyView::setName)
+                .compose()
+                .transform(Integer.class, Integer.class).supplier(Company::getEmployeeCount).consumer(CompanyView::setEmployeeCount)
+                    .finish()
+                .mapping(Address.class, AddressView.class).composer()
+                .transform(String.class, String.class).supplier(Address::getCity).consumer(AddressView::setCity)
+                .compose()
+                .transform(String.class, String.class).supplier(Address::getStreet).consumer(AddressView::setStreet)
+                .compose()
+                .transform(Integer.class, Integer.class).supplier(Address::getNumber).consumer(AddressView::setNumber)
+                    .finish()
+                .mapping(Employee.class, EmployeeView.class).composer()
+                .transform(Company.class, CompanyView.class).supplier(Employee::getCompany).consumer(EmployeeView::setCompany)
+                .function(com -> {
+                    if (com == null) {
+                        return null;
+                    }
+                    return super.getTypeMapper().map(com, CompanyView.class);
+                })
+                .compose()
+                .transform(String.class, String.class).supplier(Employee::getPosition).consumer(EmployeeView::setPosition)
+                .compose()
+                .transform(Double.class, Double.class).supplier(Employee::getSalary).consumer(EmployeeView::setSalary)
+                .compose()
+                .transform(Address.class, AddressView.class).supplier(Employee::getAddress).consumer(EmployeeView::setAddressView)
+                .function(address -> {
+                    if (address == null) {
+                        return null;
+                    }
+                    return super.getTypeMapper().map(address, AddressView.class);
+                })
+                .compose()
+                .transform(Employee.class, EmployeeView.class).supplier(Employee::getColleague).consumer(EmployeeView::setColleague)
+                .function(employee -> {
+                    if (employee == null) {
+                        return null;
+                    }
+                    return super.getTypeMapper().map(employee, EmployeeView.class);
+                })
                     .finish();
     }
 
